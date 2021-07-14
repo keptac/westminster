5ebf8900c0087489d6959d7710b263342d1ff347
Kelvin Chelenje <kelvinch@nmbz.co.zw>
2021-07-14 12:25:03 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,60 @@
+## [Techvivid Holdings - VividLearn ](https://www.techvividholding.com/)
+
+## Quick start
+
+- Make sure your NodeJS and npm versions are up to date for `React 17`
+
+- Install dependencies: `npm install` or `yarn`
+
+- Start the server: `npm run start` or `yarn start`
+
+- Views are on: `localhost:3000`
+
+## File Structure
+
+Within the download you'll find the following directories and files:
+
+```
+material-react-dashboard
+
+├── .eslintrc
+├── .gitignore
+├── .prettierrc
+├── jsconfig.json
+├── package.json
+├── README.md
+├── public
+└── src
+	├── assets
+	├── common
+	├── components
+	├── helpers
+	├── icons
+	├── layouts
+	├── theme
+	├── views
+	│	├── Account
+	│	├── Dashboard
+	│	├── Icons
+	│	├── NotFound
+	│	├── ProductList
+	│	├── Settings
+	│	├── SignIn
+	│	├── SignUp
+	│	├── Typography
+	│	└── UserList
+	├── App.jsx
+	├── index.jsx
+	└── Routes.jsx
+```
+
+
+## Reporting Issues:
+
+- [Github Issues Page](https://github.com/devias-io/react-material-dashboard/issues?ref=devias-io)
+
+## Contact Us
+
+- Email Us: keptac.dev@gmail.com
+- Email Us: keptac.dev@gmail.com
+- [Follow us on Instagram](https://www.instagram.com/techvivid/)
---
+++ src/routes.js
@@ -0,0 +1,82 @@
+import { Navigate } from 'react-router-dom';
+import DashboardLayout from 'src/components/DashboardLayout';
+import MainLayout from 'src/components/MainLayout';
+
+// Pages
+import Login from 'src/pages/Login';
+import NotFound from 'src/pages/NotFound';
+import Register from 'src/pages/Register';
+
+import Account from 'src/pages/super-admin/Account';
+import CustomerList from 'src/pages/student/CustomerList';
+import Dashboard from 'src/pages/student/Dashboard';
+import ProductList from 'src/pages/student/ProductList';
+import Settings from 'src/pages/student/Settings';
+
+const routes = [
+  // Super Admin Routes
+  {
+    path: 'app',
+    element: <DashboardLayout />,
+    children: [
+      { path: 'account', element: <Account /> },
+      { path: 'customers', element: <CustomerList /> },
+      { path: 'dashboard', element: <Dashboard /> },
+      { path: 'products', element: <ProductList /> },
+      { path: 'settings', element: <Settings /> },
+      { path: '*', element: <Navigate to="/404" /> }
+    ]
+  },
+  // Student Routes
+  {
+    path: 'student',
+    element: <DashboardLayout />,
+    children: [
+      { path: 'account', element: <Account /> },
+      { path: 'customers', element: <CustomerList /> },
+      { path: 'dashboard', element: <Dashboard /> },
+      { path: 'products', element: <ProductList /> },
+      { path: 'settings', element: <Settings /> },
+      { path: '*', element: <Navigate to="/404" /> }
+    ]
+  },
+  // Teacher Routes
+  {
+    path: 'teacher',
+    element: <DashboardLayout />,
+    children: [
+      { path: 'account', element: <Account /> },
+      { path: 'customers', element: <CustomerList /> },
+      { path: 'dashboard', element: <Dashboard /> },
+      { path: 'products', element: <ProductList /> },
+      { path: 'settings', element: <Settings /> },
+      { path: '*', element: <Navigate to="/404" /> }
+    ]
+  },
+  // School Admin Routes
+  {
+    path: 'school-admin',
+    element: <DashboardLayout />,
+    children: [
+      { path: 'account', element: <Account /> },
+      { path: 'customers', element: <CustomerList /> },
+      { path: 'dashboard', element: <Dashboard /> },
+      { path: 'products', element: <ProductList /> },
+      { path: 'settings', element: <Settings /> },
+      { path: '*', element: <Navigate to="/404" /> }
+    ]
+  },
+  {
+    path: '/',
+    element: <MainLayout />,
+    children: [
+      { path: 'login', element: <Login /> },
+      { path: 'register', element: <Register /> },
+      { path: '404', element: <NotFound /> },
+      { path: '/', element: <Navigate to="/login" /> },
+      { path: '*', element: <Navigate to="/404" /> }
+    ]
+  }
+];
+
+export default routes;
