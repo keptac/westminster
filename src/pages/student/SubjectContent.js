40ca23fec16eb13a47e4d81d1c40d36ddd90cd8f
Kelvin Chelenje <kelvinch@nmbz.co.zw>
2021-09-20 06:28:00 +0200

    documents

--- src/pages/student/SubjectContent.js
+++ src/pages/student/SubjectContent.js
@@ -1,10 +1,11 @@
-/* eslint-disable react/destructuring-assignment */
 import { Helmet } from 'react-helmet';
 import {
   Box,
   Container,
   Grid
 } from '@material-ui/core';
+
+import DocViewer, { DocViewerRenderers } from 'react-doc-viewer';
 
 import MenuBoard from 'src/components/student/StudentMenu';
 import LibraryCard from 'src/components/student/library/LibraryCard';
@@ -18,7 +19,9 @@ class SubjectContent extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      subjectName: ''
+      subjectName: '',
+      viewDoc: false,
+      docs: []
     };
   }
 
@@ -28,8 +31,15 @@ class SubjectContent extends React.Component {
     });
   }
 
+  readDocument() {
+    this.setState({
+      viewDoc: true,
+      docs: [{ uri: '../../files/monetary.pdf' }]
+    });
+  }
+
   render() {
-    const { subjectName } = this.state;
+    const { subjectName, viewDoc, docs } = this.state;
     return (
       <>
         <Helmet>
@@ -45,48 +55,65 @@ class SubjectContent extends React.Component {
           }}
         >
           <Container maxWidth={false}>
-            <Grid
-              container
-              spacing={2}
-              sx={{ marginTop: '0.1%' }}
-            >
+            {viewDoc
+              ? (
+                <Box
+                  sx={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    pt: 3
+                  }}
+                >
 
-              <Grid
-                item
-                container
-                spacing={2}
-                // sx={{ marginTop: '0.1%' }}
-                lg={10}
-                md={9}
-                xl={9}
-                xs={12}
-              >
-                {resources.map((resource) => (
+                  <DocViewer pluginRenderers={DocViewerRenderers} documents={docs} />
+                </Box>
+              )
+              : (
+                <Grid
+                  container
+                  spacing={2}
+                  sx={{ marginTop: '0.1%' }}
+                >
+
                   <Grid
                     item
-                    key={resource.id}
-                    lg={3}
-                    md={6}
+                    container
+                    spacing={2}
+                  // sx={{ marginTop: '0.1%' }}
+                    lg={10}
+                    md={9}
                     xl={9}
                     xs={12}
                   >
-                    <LibraryCard resource={resource} />
+                    {resources.map((resource) => (
+                      <Grid
+                        item
+                        key={resource.id}
+                        lg={3}
+                        md={6}
+                        xl={9}
+                        xs={12}
+                      >
+                        <div onClick={() => this.readDocument()} aria-hidden="true">
+                          <LibraryCard resource={resource} />
+                        </div>
+                      </Grid>
+                    ))}
                   </Grid>
-                ))}
-              </Grid>
 
-              <Grid
-                item
-                lg={2}
-                md={3}
-                xl={3}
-                xs={12}
-              >
-                <MenuBoard sx={{ height: '100%' }} />
+                  <Grid
+                    item
+                    lg={2}
+                    md={3}
+                    xl={3}
+                    xs={12}
+                  >
+                    <MenuBoard sx={{ height: '100%' }} />
 
-              </Grid>
+                  </Grid>
 
-            </Grid>
+                </Grid>
+              )}
           </Container>
         </Box>
       </>
