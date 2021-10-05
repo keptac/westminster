f1a30c1273b6c67ea6238ec7c6fbcd32310b04b4
keptac <keptac.dev@gmail.com>
2021-10-05 16:43:03 +0200

    registrations Integrations

--- src/services/authServices.js
+++ src/services/authServices.js
@@ -2,61 +2,30 @@ import axios from 'axios';
 
 const qs = require('qs');
 
-// const apiUrl = 'http://localhost:3001/api';
-const apiUrl = 'https://westminster-backend.herokuapp.com/api';
-
-// const token = JSON.parse(localStorage.getItem('token'));
+const apiUrl = 'http://localhost:3001/api';
+// const apiUrl = 'https://westminster-backend.herokuapp.com/api';
 
 const config = {
-  baseURL: apiUrl,
+  baseURL: `${apiUrl}/westminster`,
   headers: {
     'Content-Type': 'application/x-www-form-urlencoded',
-    // Authorization: `Bearer ${token}`,
-    // 'Access-Control-Allow-Origin': 'https://pawacyberschool.net',
-    // 'Access-Control-Allow-Credentials': true,
   },
 };
 
-// Register New User
 async function register(data) {
   try {
-    const res = await axios.post(
-      `${apiUrl}/register`,
-      qs.stringify(data),
-      config
-    );
+    const res = await axios.post('/staff', qs.stringify(data), config);
     return res.data;
   } catch (err) {
     console.error(err);
-    return err;
+    return { success: false, message: 'Connection failed. Please check your connection.' };
   }
 }
 
-// Register New User
 async function login(data) {
   try {
-    const res = await axios.post(`${apiUrl}/login`, qs.stringify(data), config);
+    const res = await axios.post('/login', qs.stringify(data), config);
     return res.data;
-  } catch (err) {
-    console.error(err);
-    return err;
-  }
-}
-
-async function profile() {
-  try {
-    const res = await axios.get('/profile', config);
-    return res;
-  } catch (err) {
-    console.error(err);
-    return err;
-  }
-}
-
-async function changePassword(data) {
-  try {
-    const res = await axios.put('/changepw', qs.stringify(data), config);
-    return res;
   } catch (err) {
     console.error(err);
     return err;
@@ -65,9 +34,7 @@ async function changePassword(data) {
 
 const AuthService = {
   register,
-  login,
-  profile,
-  changePassword,
+  login
 };
 
 export default AuthService;
